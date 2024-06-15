2b9cc78d28a5d47b62375a4fee8b02daa7d2ecf8
Shubham Girase <sgirase08@gmail.com>
2024-06-15 17:52:21 +0530

    Wrap logo to navigate home

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,9 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <div className="flex justify-between items-center h-16">
-      <h2 className="font-semibold">BookShelf</h2>
+      <Link to="/">
+        <h2 className="font-semibold">BookShelf</h2>
+      </Link>
 
       <Link to="/bookshelf">
         <h2 className="text-sm font-semibold text-gray-600 hover:text-gray-800">
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,28 +1,33 @@
-import { useState } from "react";
+import { useState, useCallback, useEffect } from "react";
+
 import SearchInput from "../components/SearchInput";
 import BookList from "../components/BookList";
-import { useCallback } from "react";
-import { useEffect } from "react";
-
-const SEARCH_API = `https://openlibrary.org/search.json?q=YOUR_QUERY&limit=10&page=1`;
-const COVER_API = `https://covers.openlibrary.org/b/id/258027-M.jpg`;
 
 const Home = () => {
   const [searchQuery, setSearchQuery] = useState("");
   const [debouncedQuery, setDebouncedQuery] = useState("");
   const [bookData, setBookData] = useState([]);
+  const [isLoading, setLoading] = useState(false);
 
-  const fetchBooks = useCallback(async (query) => {
-    try {
-      const res = await fetch(
-        `https://openlibrary.org/search.json?q=${query}&limit=10&page=1`
-      );
-      const data = await res.json();
-      setBookData(data.docs);
-    } catch (error) {
-      console.error("Error fetching data:", error);
-    }
-  }, []);
+  const fetchBooks = useCallback(
+    async (query) => {
+      if (searchQuery) {
+        try {
+          setLoading(true);
+          const res = await fetch(
+            `https://openlibrary.org/search.json?q=${query}&limit=10&page=1`
+          );
+          const data = await res.json();
+          setBookData(data.docs);
+        } catch (error) {
+          console.error("Error fetching data:", error);
+        } finally {
+          setLoading(false);
+        }
+      }
+    },
+    [searchQuery]
+  );
 
   useEffect(() => {
     const handler = setTimeout(() => {
@@ -39,9 +44,19 @@ const Home = () => {
   }, [debouncedQuery, fetchBooks]);
 
   return (
-    <div className="mx-auto space-y-4">
+    <div className="mx-auto space-y-6">
       <SearchInput onChange={setSearchQuery} searchQuery={searchQuery} />
-      <BookList bookData={bookData} />
+      {isLoading ? (
+        <p className="text-center">Loading... </p>
+      ) : (
+        <BookList bookData={bookData} />
+      )}
+
+      {searchQuery?.length == 0 && (
+        <p className="text-center text-sm text-gray-600">
+          Try to search book by title
+        </p>
+      )}
     </div>
   );
 };
